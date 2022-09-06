5aa50172eccb879c8915e65abf886d4da5262446
Francisco Ylderlan <108945231+FranciscoYlderlan@users.noreply.github.com>
2022-09-06 19:08:32 -0300

    audios convertidos

--- js/sounds.js
+++ js/sounds.js
@@ -1,10 +1,9 @@
 export const sound = {
     btnPress: new Audio("https://github.com/FranciscoYlderlan/stage-5-focustime-2.0/blob/main/assets/audios_button-press.wav?raw=true"),
-    music: new Audio("https://github.com/FranciscoYlderlan/stage-5-focustime-2.0/blob/main/assets/audios_lo-fi.mp3?raw=true"),
-    forest: new Audio("https://github.com/FranciscoYlderlan/stage-5-focustime-2.0/blob/main/assets/Floresta.wav?raw=true"),
-    clounds: new Audio("https://github.com/FranciscoYlderlan/stage-5-focustime-2.0/blob/main/assets/Chuva.wav?raw=true"),
-    coffeeTea: new Audio("https://github.com/FranciscoYlderlan/stage-5-focustime-2.0/blob/main/assets/Cafeteria.wav?raw=true"),
-    bonFire: new Audio("https://github.com/FranciscoYlderlan/stage-5-focustime-2.0/blob/main/assets/Lareira.wav?raw=true"),
+    forest: new Audio("https://github.com/lfoalves/material-stage-05/blob/master/sounds/Floresta.mp3?raw=true"),
+    clounds: new Audio("https://github.com/lfoalves/material-stage-05/blob/master/sounds/Chuva.mp3?raw=true"),
+    coffeeTea: new Audio("https://github.com/lfoalves/material-stage-05/blob/master/sounds/Cafeteria.mp3?raw=true"),
+    bonFire: new Audio("https://github.com/lfoalves/material-stage-05/blob/master/sounds/Lareira.mp3?raw=true"),
 
     kitchenTimer: new Audio("https://github.com/FranciscoYlderlan/stage-5-focustime-2.0/blob/main/assets/audios_kitchen-timer.mp3?raw=true"), 
     
